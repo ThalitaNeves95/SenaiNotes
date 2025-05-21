3fcd2d7253d95d9ba4eaca650c5e3af6dd10e658
Gustavo Oliveira <graataogustavo@gmail.com>
2025-05-21 18:36:02 -0300

    Usuario ADMIN Pronto

---
+++ SenaiNotes/Users/UsuarioSeed.cs
@@ -0,0 +1,34 @@
+﻿using APISenaiNotes.Models;
+using Microsoft.EntityFrameworkCore;
+using SenaiNotes.Context;
+namespace APISenaiNotes.Users
+{
+
+
+    public static class SeedData
+    {
+        public static void Inicializar(IServiceProvider serviceProvider)
+        {
+            var options = serviceProvider.GetRequiredService<DbContextOptions<SenaiNotesContext>>();
+            var config = serviceProvider.GetRequiredService<IConfiguration>();
+
+            var context = new SenaiNotesContext(options, config);
+            {
+                
+                if (!context.Usuarios.Any())
+                {
+                    context.Usuarios.Add(new Usuario
+                    {
+                        UsuarioId = 1,
+                        NomeUsuario = "admin",
+                        Email = "admin@senai.com",
+                        Senha = "admin123",
+                        DataCriacao = DateTime.Now
+                    });
+
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
